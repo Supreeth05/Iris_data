46e57ff4ea85a05485cc11973647fd5df808d3be
Supreeth05 <71519782+Supreeth05@users.noreply.github.com>
2021-04-25 22:12:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,13 +17,13 @@ def predict():
     sepal_length = float(request.form['sepal_length'])
     sepal_width = float(request.form['sepal_width'])
     petal_length = float(request.form['petal_length'])
-    petal_width = request.form['petal_width']
+    petal_width = float(request.form['petal_width'])
     finalFeatures = np.concatenate((np.array([[sepal_length,sepal_width,petal_length,petal_width]])) , axis = 1)
     prediction = model.predict(finalFeatures)
 
     
 
-    return render_template('index.html', prediction_text='Prediction  $ {}'.format(round(prediction[0][0])))
+    return render_template('index.html', prediction_text='Prediction  $ {}'.format(round(prediction[0])))
 
 
 if __name__ == "__main__":
